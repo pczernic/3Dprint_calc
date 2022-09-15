a67fa9bc63c25fda9e240a36378d0f9c0261f2cf
Pawel <p.czernic98@gmail.com>
2022-09-15 23:02:58 +0200

    Added price calculation.

--- classes.py
+++ classes.py
@@ -1,5 +1,4 @@
 import stl
-from stl import mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot
 
@@ -29,6 +28,7 @@ class MeshObject:
         self.volume = 0
         self.figure = None
         self.axes = None
+        self.scale = None
 
     def get_volume(self):
         self.volume, _, inertia = self.mesh.get_mass_properties()
@@ -48,9 +48,84 @@ class MeshObject:
         # Vectors to plot
         self.axes.add_collection3d(mplot3d.art3d.Poly3DCollection(self.mesh.vectors))
 
-        # Auto scale to the mesh size
+        # Auto-scale to the mesh size
         self.scale = self.mesh.points.flatten()
         self.axes.auto_scale_xyz(self.scale, self.scale, self.scale)
 
         # Show the plot to the screen
-        pyplot.show()
+        # pyplot.show()
+
+
+class Prize:
+    def __init__(self, volume, dimensions):
+        self.finalprize = None
+        self.material_f = None
+        self.quality_f = None
+        self.material = None
+        self.thickness = None
+        self.realvol = None
+        self.materials = None
+        self.volume = volume
+        self.dimensions = dimensions
+        self.infill = None
+
+    def possibility(self):
+        print('Można drukować z:')
+        if self.dimensions[0] < 200 and self.dimensions[1] < 200 and self.dimensions[2] < 200:
+            self.materials = ['PLA', 'PETG', 'ASA', 'ABS', 'TPU']
+            print(self.materials)
+        elif self.dimensions[0] < 400 and self.dimensions[1] < 400 and self.dimensions[2] < 400:
+            self.materials = ['PLA', 'PETG']
+            print(self.materials)
+        else:
+            print('Uwaga wydruk wielkoformatowy - prosimy o kontakt.')
+        return self.materials
+
+    def realvolume(self, infill):
+        self.infill = infill
+        self.realvol = self.volume * (0.2 + 0.8 * self.infill)
+        return self.realvol
+
+    def choose_quality(self, thickness):
+        self.thickness = thickness
+        if self.thickness == 0.28:
+            self.quality_f = 0.8
+            return self.quality_f
+        elif self.thickness == 0.2:
+            self.quality_f = 1
+            return self.quality_f
+        elif self.thickness == 0.16:
+            self.quality_f = 1.25
+            return self.quality_f
+        elif self.thickness == 0.12:
+            self.quality_f = 1.5
+            return self.quality_f
+        else:
+            print('zla wartość')
+            return None
+
+    def choose_material(self, material):
+        self.material = material
+        if self.material == 'PLA':
+            self.material_f = 1
+            return self.material_f
+        elif self.material == 'PETG':
+            self.material_f = 1.25
+            return self.material_f
+        elif self.material == 'ASA':
+            self.material_f = 1.5
+            return self.material_f
+        elif self.material == 'ABS':
+            self.material_f = 1.75
+            return self.material_f
+        elif self.material == 'TPU':
+            self.material_f = 2
+            return self.material_f
+        else:
+            print('zly material')
+            return None
+
+    def final_prize_calc(self):
+        self.finalprize = 10 + (self.realvol / 1000 * self.quality_f * self.material_f)
+        return self.finalprize
+
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from classes import StlObject, MeshObject
+from classes import StlObject, MeshObject, Prize
 
 
 if __name__ == '__main__':
-    model = StlObject('playing_card_holder.stl')
+    model = StlObject('10mmx3.stl')
     mesh = MeshObject(model.stl2mesh())
     vol = mesh.get_volume()
     dims = mesh.get_dimensions()
-    print(vol)
-    print(dims)
+    print('volume: {}'.format(vol))
+    print('dimensions: {}'.format(dims))
     mesh.show_object()
+
+    calc = Prize(vol, dims)
+    calc.possibility()
+    realvol = calc.realvolume(1)
+    quality = calc.choose_quality(0.12)
+    material = calc.choose_material('PLA')
+    final_prize = calc.final_prize_calc()
+    print(final_prize)
+
+
 
 
 
